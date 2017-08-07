66b2510119ab71d82f0679cc3480dab05f08fc33
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-08-06 23:58:13 -0400

    added projects, users table creation; still need to debug

--- bq-api.go
+++ bq-api.go
@@ -1,24 +1,56 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"cloud.google.com/go/bigquery"
-	"golang.org/x/net/context"
+  "fmt"
+  "log"
+
+  "net/http"
+
+  "cloud.google.com/go/bigquery"
+  "golang.org/x/net/context"
 )
 
-func main() {
-	ctx := context.Background()
-	projectID := "synergist-170903"
-	client, err := bigquery.NewClient(ctx, projectID)
-        if err != nil {
-                log.Fatalf("Failed to create client: %v", err)
-        }
-	datasetName := "ngs_dataset"
-	dataset := client.Dataset(datasetName)
-	if err := dataset.Create(ctx); err != nil {
-                log.Fatalf("Failed to create dataset: %v", err)
-        }
+var CONTEXT = context.Background()
 
-        fmt.Printf("Dataset created\n")
+func main() {
+  client := getBqClient()
+  projects, users := getTables(client, "synset_prod")
+  fmt.Println(projects, users)
+  http.ListenAndServe(":8080", nil)
+}
+
+func getBqClient() (*bigquery.Client) {
+  projectID := "synergist-170903"
+  client, err := bigquery.NewClient(CONTEXT, projectID)
+  if err != nil {
+    log.Fatalf("Failed to create client: %v", err)
+  }
+  return client
+}
+
+func getTables(client *bigquery.Client, datasetName string) (*bigquery.Client.Dataset.Table, *bigquery.Client.Dataset.Table) {
+  md, err := client.Dataset(datasetName).Metadata(CONTEXT)
+  if err != nil { // create dataset
+    if err := client.Dataset(datasetName).create(CONTEXT); err != nil {
+      log.Fatalf("Failed to create dataset: %v", err)
+    }
+  }
+
+  md, err = client.Dataset(datasetName).Table("projects").Metadata(CONTEXT)
+  if err != nil { // create projects table
+    if err := client.Dataset(datasetName).Table("projects").Create(CONTEXT); err != nil {
+      log.Fatalf("Failed to create 'projects' table: %v", err)
+    }
+  }
+
+  md, err = client.Dataset(datasetName).Table("users").Metadata(CONTEXT)
+  if err != nil { // create users table
+    if err := client.Dataset(datasetName).Table("users").Create(CONTEXT); err != nil {
+      log.Fatalf("Failed to create 'users' table: %v", err)
+    }
+  }
+
+  projects := client.Dataset(datasetName).Table("projects")
+  users := client.Dataset(datasetName).Table("users")
+  return projects, users
 }
