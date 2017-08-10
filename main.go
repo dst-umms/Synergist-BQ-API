121c60c7d6add19b9949ce2314c4e9e11efdb88b
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-08-10 13:31:23 -0400

    added proper logging

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "log"
+  "os"
   "encoding/json"
 
   "net/http"
@@ -14,6 +15,12 @@ import (
   "./packages/schema"
 )
 
+// Log handling
+var Error *log.Logger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+var Warn *log.Logger = log.New(os.Stdout, "WARN: ", log.Ldate|log.Ltime|log.Lshortfile)
+var Info *log.Logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+// BigQuery globals 
 var CONTEXT = context.Background()
 var projects, users *bigquery.Table
 const BQ_DATASET string = "devel"
@@ -30,7 +37,8 @@ func getBqClient() (*bigquery.Client) {
   projectID := "synergist-170903"
   client, err := bigquery.NewClient(CONTEXT, projectID)
   if err != nil {
-    log.Fatalf("Failed to create client: %v", err)
+    Error.Printf("Failed to create client: %v", err)
+    os.Exit(1)
   }
   return client
 }
@@ -38,7 +46,8 @@ func getBqClient() (*bigquery.Client) {
 func getTables(client *bigquery.Client, datasetName string) (*bigquery.Table, *bigquery.Table) {
   if  _, err := client.Dataset(datasetName).Metadata(CONTEXT); err != nil {
     if err := client.Dataset(datasetName).Create(CONTEXT); err != nil {
-      log.Fatalf("Failed to create dataset: %v", err)
+      Error.Printf("Failed to create dataset: %v", err)
+      os.Exit(1)
     }
   }
 
@@ -49,13 +58,15 @@ func getTables(client *bigquery.Client, datasetName string) (*bigquery.Table, *b
 
   if _, err := projects.Metadata(CONTEXT); err != nil {
     if err := projects.Create(CONTEXT, projectSchema); err != nil {
-      log.Fatalf("Failed to create 'projects' table: %v", err)
+      Error.Printf("Failed to create 'projects' table: %v", err)
+      os.Exit(1)
     }
   }
 
   if _, err := users.Metadata(CONTEXT); err != nil {
     if err := users.Create(CONTEXT, userSchema); err != nil {
-      log.Fatalf("Failed to create 'users' table: %v", err)
+      Error.Printf("Failed to create 'users' table: %v", err)
+      os.Exit(1)
     }
   }
 
@@ -69,13 +80,14 @@ func loadProjectData(res http.ResponseWriter, req *http.Request) {
   var projectData schema.Project
   _ = decoder.Decode(&projectData)
   if err := u.Put(CONTEXT, projectData); err != nil {
-    log.Fatalf("Failed to load project data: %v", err)
     res.WriteHeader(http.StatusInternalServerError)
     res.Write([]byte("500 - Failed to save Project Data!"))
+    Error.Printf("Failed to save project data: %v", projectData)
+  } else {
+    Info.Printf("Saved project data: %v", projectData)
+    res.WriteHeader(http.StatusOK)
+    res.Write([]byte("Success!"))
   }
-  fmt.Println(projectData)
-  res.WriteHeader(http.StatusOK)
-  res.Write([]byte("Success!"))
   defer req.Body.Close()
 }
 
@@ -85,12 +97,13 @@ func loadUserData(res http.ResponseWriter, req *http.Request) {
   var userData schema.User
   _ = decoder.Decode(&userData)
   if err := u.Put(CONTEXT, userData); err != nil {
-    log.Fatalf("Failed to load user data: %v", err)
     res.WriteHeader(http.StatusInternalServerError)
     res.Write([]byte("500 - Failed to save User Data!"))
+    Error.Printf("Failed to save user data: %v", userData)
+  } else {
+    Info.Printf("Saved user data: %v", userData)
+    res.WriteHeader(http.StatusOK)
+    res.Write([]byte("Success!"))
   }
-  fmt.Println(userData)
-  res.WriteHeader(http.StatusOK)
-  res.Write([]byte("Success!"))
   defer req.Body.Close()
 }
